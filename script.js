ee95af07b569122464be76211c7fb9186087b272
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-22 13:45:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let number = 0
+const value = 0
+let base = 0
+let width = 0
+let height = 0 
+let area = 0
+
+
+document.getElementById('button').addEventListener('click', addition)
+document.getElementById('button').addEventListener('click', add)
+document.getElementById('divide').addEventListener('click', divide)
+document.getElementById('multiply').addEventListener('click',multi)
+document.getElementById('multiply').addEventListener('click',ply)
+
+function addition () {
+  base = document.getElementById('add').value
+  base = parseInt(base)
+  number = base + width
+}
+
+function add () {
+  width = document.getElementById('addition').value
+  width = parseInt(width)
+  number = base + width
+  alert (number)
+}
+
+function divide () {
+  width = document.getElementById('box').value
+  width = parseInt(width)
+  number = width / 2
+  alert (number)
+}
+
+function multi () {
+  width = document.getElementById('multi').value
+  width = parseInt(width)
+  number = width * height
+}
+
+function ply () {
+  height = document.getElementById('ply').value
+  height = parseInt(height)
+  number = width * height 
+  document.getElementById('para').innerHTML = 
+  'Final Result is below' 
+  document.getElementById('paragraph').innerHTML =
+  number
+}
+
+
