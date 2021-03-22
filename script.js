4f655559ef3792672f7ddb0e1f0d93bc0f322398
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-22 14:14:09 +0000

    Final Final Final Errors JS

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let number = 0
 let base = 0
 let width = 0
-let height = 0 
+let height = 0
 
 document.getElementById('button').addEventListener('click', addition)
 document.getElementById('button').addEventListener('click', add)
 document.getElementById('divide').addEventListener('click', divide)
 document.getElementById('multiply').addEventListener('click', multi)
-document.getElementById('multiply').addEventListener('click',ply)
+document.getElementById('multiply').addEventListener('click', ply)
 
 function addition () {
   base = document.getElementById('add').value
@@ -19,14 +19,14 @@ function add () {
   width = document.getElementById('addition').value
   width = parseInt(width)
   number = base + width
-  alert (number)
+  alert(number)
 }
 
 function divide () {
   width = document.getElementById('box').value
   width = parseInt(width)
   number = width / 2
-  alert (number)
+  alert(number)
 }
 
 function multi () {
@@ -38,11 +38,9 @@ function multi () {
 function ply () {
   height = document.getElementById('ply').value
   height = parseInt(height)
-  number = width * height 
-  document.getElementById('para').innerHTML = 
-  'Final Result is below' 
+  number = width * height
+  document.getElementById('para').innerHTML =
+  'Final Result is below'
   document.getElementById('paragraph').innerHTML =
   number
 }
-
-
